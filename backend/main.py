6b5ba4d53787bd53202e25026117bfbe6f428d0d
pestcide <chenyuhwang@foxmail.com>
2025-11-16 11:56:01 +0800

    Video aligned

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from moviepy import VideoFileClip
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
@@ -6,6 +7,7 @@ from PIL import Image
 from io import BytesIO
 import os
 from glob import glob
+import numpy as np
 
 app = FastAPI()
 
@@ -43,6 +45,35 @@ def get_compressed_photo(filename: str, max_width: int = 1200):
     img.save(buf, format="JPEG", optimize=True, quality=85)
     buf.seek(0)
     return StreamingResponse(buf, media_type="image/jpeg")
+
+@app.get("/thumbnail")
+def get_thumbnail(video: str = Query(...)):
+    print(video)
+    video_path = os.path.join("media", "videos", video)
+    if not os.path.exists(video_path):
+        print("Video not found")
+        return {"error": "Video not found"}
+
+    clip = VideoFileClip(video_path)
+    # frame = clip.get_frame(1)  # 获取1秒处的帧 (numpy array)
+    # 获取第一帧（t=0）
+    frame = clip.get_frame(0)
+
+    # 确保为 uint8 并去掉可能的 alpha 通道
+    frame = np.asarray(frame)
+    if frame.dtype != np.uint8:
+        frame = np.clip(frame, 0, 255).astype(np.uint8)
+    if frame.ndim == 3 and frame.shape[2] == 4:
+        frame = frame[:, :, :3]
+    clip.close()
+
+    # 转为 PIL Image
+    img = Image.fromarray(frame)
+    buffer = BytesIO()
+    img.save(buffer, format="JPEG")
+    buffer.seek(0)
+
+    return StreamingResponse(buffer, media_type="image/jpeg")
 
 # 动态返回媒体列表
 @app.get("/media")
